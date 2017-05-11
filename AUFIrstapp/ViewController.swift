74bfd57f2a3fb3bb2569154c8bc5923c0623bd16
Mumin Uddin <abduluddin@Mumins-MacBook-Pro.local>
2017-05-11 19:14:46 -0400

    Initial Commit

---
+++ AUFIrstapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AUFIrstapp
+//
+//  Created by Mumin Uddin on 5/11/17.
+//  Copyright © 2017 Mumin Uddin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
